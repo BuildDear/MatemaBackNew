3d404be77c4a0dcd111a77eb989cd67087396d85
Yura0Bodnar <yuriy575721@gmail.com>
2023-11-11 20:02:37 +0200

    nothing

--- User/serializers.py
+++ User/serializers.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.password_validation import validate_password
 from django.db import transaction
 from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer
 from .models import User
+from djoser.serializers import UserCreateSerializer
 
 
 class UserListSerializer(serializers.ModelSerializer):
