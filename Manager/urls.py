5b798ec7d8ba44c2cb1cf071723a852149f67d88
marik <letschuka@gmail.com>
2023-11-17 10:14:03 +0200

    change manager urls

--- Manager/urls.py
+++ Manager/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('all/', TasksView.as_view(), name='view-task'),
-    path('create/', TaskCreateView.as_view(), name='create-task'),
-    path('delete/<int:task_id>/', TaskDeleteView.as_view(), name='delete-task'),
-    path('task/<int:pk>/', TaskEditView.as_view(), name='task-edit'),
+    path('task/all/', TasksView.as_view(), name='view-task'),
+    path('task/create/', TaskCreateView.as_view(), name='create-task'),
+    path('task/delete/<int:pk>/', TaskDeleteView.as_view(), name='delete-task'),
+    path('task/edit/<int:pk>/', TaskEditView.as_view(), name='task-edit'),
 
-    path('theme/', ThemeView.as_view(), name='view-theme'),
-    path('theme_create/', ThemeCreateView.as_view(), name='create-theme'),
-    path('theme/<int:theme_id>/', ThemeDeleteView.as_view(), name='delete-theme'),
-    path('theme/<int:pk>/', ThemeEditView.as_view(), name='task-edit'),
+    path('theme/all/', ThemeView.as_view(), name='view-theme'),
+    path('theme/create/', ThemeCreateView.as_view(), name='create-theme'),
+    path('theme/delete/<int:pk>/', ThemeDeleteView.as_view(), name='delete-theme'),
+    path('theme/edit/<int:pk>/', ThemeEditView.as_view(), name='task-edit'),
 ]
