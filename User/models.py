8bd43a6d83815d6f5bdac031796e53a57bad567d
marik <letschuka@gmail.com>
2023-11-21 19:26:49 +0200

    delete all migrations

--- User/models.py
+++ User/models.py
@@ -2,12 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class User(AbstractUser):
     image_url = models.CharField(max_length=255, default='None')
     score = models.IntegerField(default=0)
     count_tasks = models.IntegerField(default=0)
-
 
     REQUIRED_FIELDS = ('email', 'first_name', 'last_name', 'password')
 
