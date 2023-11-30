a35c3db6c21869c32d4e2c3ae62f97f8f230ffde
marik <letschuka@gmail.com>
2023-11-30 23:27:33 +0200

    add Task to docker

--- Task/admin.py
+++ Task/admin.py
@@ -5,28 +5,36 @@ from .models import TypeAnswer, Theme, Task, TaskList, UserTheme, DoneTask
 admin.site.register(TypeAnswer)
 admin.site.register(Theme)
 
+
 # Створення класів адміністратора для кастомізації відображення
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('name', 'point', 'theme', 'type_ans')  # вказуємо поля, які хочемо бачити в списку
     search_fields = ('name', 'theme__name')  # додаємо можливість пошуку за цими полями
 
+
 admin.site.register(Task, TaskAdmin)
+
 
 class TaskListAdmin(admin.ModelAdmin):
     list_display = ('user', 'task', 'is_weekly')
     search_fields = ('user__username', 'task__name')
 
+
 admin.site.register(TaskList, TaskListAdmin)
+
 
 class UserThemeAdmin(admin.ModelAdmin):
     list_display = ('user', 'theme')
     search_fields = ('user__username', 'theme__name')
 
+
 admin.site.register(UserTheme, UserThemeAdmin)
+
 
 class DoneTaskAdmin(admin.ModelAdmin):
     list_display = ('user', 'task', 'is_done', 'datetime')
     search_fields = ('user__username', 'task__name')
     list_filter = ('is_done',)  # додаємо фільтр за полем is_done
+
 
 admin.site.register(DoneTask, DoneTaskAdmin)
