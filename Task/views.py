8e2c28ce857c0b5f5be3062696cb8f7925ea8e78
marik <letschuka@gmail.com>
2023-12-11 23:28:37 +0200

    In Task app add logic transfering task to table DoneTask correctly

--- Task/views.py
+++ Task/views.py
@@ -9,24 +9,19 @@ from Task.serializer import *
 class TaskListView(APIView):
     permission_classes = (AllowAny,)
 
-    def post(self, request):
-        username = request.data.get('username')
-        # Перевірка, чи вказано username
+    def get(self, request, username):
+
         if not username:
             return Response({"error": "Username is required."}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            # Створення списку завдань для користувача
             tasklist = create_tasklist(username)
-
-            # Серіалізація даних списку завдань
             serializer = TaskListSerializer(tasklist, many=True)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         except User.DoesNotExist:
             return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            # Для відловлення інших можливих помилок
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
