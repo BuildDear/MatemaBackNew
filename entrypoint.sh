c82ddcb51f44e57684fccf493c22111c77f7d886
marik <letschuka@gmail.com>
2023-12-29 14:08:56 +0200

    update Dockerfile and docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-# Зупинити виконання скрипту при помилці
 set -e
 
-# Створення міграцій
 python manage.py makemigrations --noinput
 
-# Виконання міграцій
 python manage.py migrate --noinput
 
-# Запуск сервера Django
 exec python manage.py runserver 0.0.0.0:8000
